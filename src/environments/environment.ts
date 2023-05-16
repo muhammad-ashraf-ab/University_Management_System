b0d5ffb442797d20c9ae94df0368287dfe1601f0
“Muhammad <modogogo2000@hotmail.com>
2023-05-16 12:12:34 +0300

    Project init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'cse379-project-ums',
+    appId: '1:172186509703:web:db4ed593a9e1a44ea67322',
+    databaseURL: 'https://cse379-project-ums-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'cse379-project-ums.appspot.com',
+    apiKey: 'AIzaSyCBjZjbbJUuBCWp8lkNI4uWhmlrQk5jdcc',
+    authDomain: 'cse379-project-ums.firebaseapp.com',
+    messagingSenderId: '172186509703',
+  }
 };
 
 /*
